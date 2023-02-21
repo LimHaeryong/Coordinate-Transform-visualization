38f5c9f1e334597e945da58b87d85264a8a01c35
haeryong <nabi4622@snu.ac.kr>
2023-02-21 17:14:19 +0900

    first commit

---
+++ examples/main.cpp
@@ -0,0 +1,14 @@
+
+#include <pangolin/pangolin.h>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "main" << std::endl;
+    return 0;
+}
+
